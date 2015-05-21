36784f71e9db50ac9494cee1c300ab5b7966eca0
matthijs2704 <matthijs.logemann@xs4all.nl>
2015-05-21 17:06:37 +0200

    Adding Parse...

--- WWDC Scholars 2015/WWDC Scholars 2015/AppDelegate.swift
+++ WWDC Scholars 2015/WWDC Scholars 2015/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("***REMOVED***",
+            clientKey: "***REMOVED***")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
+        DataManager.sharedInstance.loadStudents()
         // Override point for customization after application launch.
         return true
     }
