cb6e8433f76900c994cdde190266717d918935b0
matthijs2704 <matthijs.logemann@xs4all.nl>
2015-05-21 16:49:56 +0200

    Added UICollectionView to the main view

---
+++ WWDC Scholars 2015/WWDC Scholars 2015/MainScreenViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainScreenViewController.swift
+//  WWDC Scholars 2015
+//
+//  Created by Matthijs Logemann on 21-05-15.
+//  Copyright (c) 2015 WWDC-Scholars. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ScholarshipCell", forIndexPath: indexPath) as! UICollectionViewCell
+        cell.backgroundColor = UIColor.orangeColor()
+        
+        cell.layer.cornerRadius = 10
+        cell.layer.masksToBounds = true
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+
+}
