c55492297a4e4cd49b97474a8e6523be316b8a58
harshita-gupta <gupta.harshita.001@gmail.com>
2015-05-23 10:51:08 -0700

    Contact Buttons Implemented (except email)
    Facebook, GH, Twitter, buttons working. If no link provided, displays
    message to that extent.
    
    To do: collection view that shows screenshots. Make it nicer visually.

--- WWDC Scholars 2015/WWDC Scholars 2015/Controller/DetailViewController.swift
+++ WWDC Scholars 2015/WWDC Scholars 2015/Controller/DetailViewController.swift
@@ -12,36 +12,96 @@ class DetailViewController: UIViewController, UICollectionViewDataSource, UIColl
     
     var currentScholar : Scholar?
     
+    var popUpView : UIView?
+    
+    var errorLabel: UILabel?
+    
     @IBOutlet private weak var imgScholar: AsyncImageView!
     
     @IBOutlet private weak var btnGithubRepo: UIButton!
     
     @IBOutlet weak var shortBioLabel: UILabel!
     
-    
-    
     @IBOutlet weak var descriptionLabel: UILabel!
     
     @IBOutlet weak var mapView: MKMapView!
     
     @IBAction func toGithub(sender: UIButton) {
+        if (currentScholar?.githubLinkToApp != nil) {
+            UIApplication.sharedApplication().openURL(NSURL(string:currentScholar!.githubLinkToApp!)!)
+        }
+        
+        else {
+            errorLabel?.text = currentScholar!.name! + " has not submitted their GitHub link. Email them and ask them to!"
+            appearDisappear(popUpView!)
+        }
     }
     
     @IBAction func emailClicked(sender: UIButton) {
     }
     
     
-    @IBAction func facebookClied(sender: UIButton) {
+    @IBAction func facebookClicked(sender: AnyObject) {
+        if (currentScholar?.facebook != nil) {
+            UIApplication.sharedApplication().openURL(NSURL(string:currentScholar!.facebook!)!)
+        }
+        else {
+            errorLabel?.text = currentScholar!.name! + " has not submitted their Facebook link. Email them and ask them to!"
+            appearDisappear(popUpView!)
+        }
+
     }
     
-    @IBOutlet weak var twitterClicked: UIButton!
     
+    @IBAction func twitterClicked(sender: AnyObject) {
+        if (currentScholar?.twitter != nil) {
+        UIApplication.sharedApplication().openURL(NSURL(string:currentScholar!.twitter!)!)
+        }
+        
+        else {
+            errorLabel?.text = currentScholar!.name! + " has not submitted their Twitter link. Email them and ask them to!"
+            appearDisappear(popUpView!)
+        }
+
+    }
     
+    func appearDisappear (view: UIView) {
+        view.hidden = false
+        view.alpha = 1.0
+        UIView.animateWithDuration(0.2 as NSTimeInterval, delay: 2.0 as NSTimeInterval, options: UIViewAnimationOptions.AllowUserInteraction, animations: {view.alpha = 0.0}, completion: { (value: Bool) in view.hidden = true })
+    }
+    
+    func configureErrorPopUp() {
+        popUpView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width - 60, height: 50))
+        popUpView?.frame.origin.x = (self.view.frame.size.width - popUpView!.frame.size.width ) / 2
+        popUpView?.frame.origin.y = (self.view.frame.size.height  - popUpView!.frame.size.height) / 2
+        
+        popUpView?.backgroundColor = UIColor(red: (170.0/255.0), green: (170.0/255.0), blue: (170.0/255.0), alpha: 0.8)
+        
+        popUpView!.layer.cornerRadius = 5
+        popUpView!.layer.masksToBounds = true
+        
+        self.errorLabel = UILabel(frame: CGRect(x : 0, y : 0, width : popUpView!.frame.size.width, height : popUpView!.frame.size.height))
+        
+        errorLabel?.numberOfLines = 2
+        errorLabel?.textAlignment = NSTextAlignment.Center
+        
+        errorLabel!.font = UIFont(name: "Helvetica Neue", size: 12)
+        errorLabel!.textColor = UIColor.blackColor()
+        
+        popUpView?.addSubview(errorLabel!)
+        
+        self.view.addSubview(popUpView!)
+        popUpView?.hidden = true
+    
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureErrorPopUp()
+        
         let attributes = [NSFontAttributeName : UIFont(name: "HelveticaNeue-Medium", size: 19)!, NSForegroundColorAttributeName : UIColor(red: 0.30980392156862746, green: 0.69411764705882351, blue: 0.90196078431372551, alpha: 1)]
         
         self.navigationController?.navigationBar.titleTextAttributes = attributes
