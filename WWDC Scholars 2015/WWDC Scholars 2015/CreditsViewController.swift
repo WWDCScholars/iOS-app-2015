3f2cf3cc2ea9cb62f944824e9bcaaa3d5a11679a
Andrew-Walker <andreww500@live.com>
2015-05-25 23:35:15 +0100

    Added link to website button in credits.

--- WWDC Scholars 2015/WWDC Scholars 2015/CreditsViewController.swift
+++ WWDC Scholars 2015/WWDC Scholars 2015/CreditsViewController.swift
@@ -22,6 +22,10 @@ class CreditsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func openWebsite(sender: AnyObject) {
+        UIApplication.sharedApplication().openURL(NSURL(string: "http://wwdcscholars.com")!)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
