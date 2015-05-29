b03bad2c37a83d9bf3dba90c2c3d5991d654578b
Grey Chen <geleichen@gmail.com>
2015-05-28 22:35:31 -0400

    Fix the correct position for button

--- WWDC Scholars 2015/WWDC Scholars 2015/LocationViewController.swift
+++ WWDC Scholars 2015/WWDC Scholars 2015/LocationViewController.swift
@@ -51,7 +51,7 @@ class LocationViewController: UIViewController,CLLocationManagerDelegate,MKMapVi
         //The "Find me" button
         let button = UIButton.buttonWithType(UIButtonType.Custom) as! UIButton
         //println(self.tableView.frame.maxY)
-        button.frame = CGRectMake(UIScreen.mainScreen().bounds.width - 55,70, 50, 50)
+        button.frame = CGRectMake(UIScreen.mainScreen().bounds.width - 55,10, 50, 50)
         button.setImage(UIImage(named: "MyLocation"), forState: .Normal)
         button.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
         button.layer.shadowOpacity = 0.5
